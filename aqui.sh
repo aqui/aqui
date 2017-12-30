d2d025e2bc966a20ac7a96add5809d1826d34f68
aqui <akifbatur@protonmail.com>
2017-12-30 22:54:34 +0300

    generated artifacts with base settings

--- aqui.sh
+++ aqui.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+rm -r channel-artifacts
+
+mkdir channel-artifacts
+
+export FABRIC_CFG_PATH=.
+export CHANNEL_NAME="mychannel"
+export CLI_TIMEOUT=10
+export CLI_DELAY=3
+export COMPOSE_FILE=docker-compose-cli.yaml
+export LANGUAGE=golang
+
+# Generate certificates using cryptogen tool
+cryptogen generate --config=./crypto-config.yaml
+
+# Generate Orderer Genesis block
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+
+# Generating channel configuration transaction channel.tx
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+
+# Generating anchor peer update for Org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+# Generating anchor peer update for Org2MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
